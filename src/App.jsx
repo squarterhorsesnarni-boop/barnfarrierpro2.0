9aaa862197632f1492305a95df9e801e2d58d9a9
Alessandro Brussard <147909450+alebrussard07@users.noreply.github.com>
2026-04-20 20:03:12 +0200

    fix horseform import path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
   calculateStatus,
   formatDate,
 } from './lib/dateUtils';
-import HorseForm from './components/horseform';
+import HorseForm from './components/horseform.jsx';
 import Toast from './components/Toast';
 import './index.css';
 
